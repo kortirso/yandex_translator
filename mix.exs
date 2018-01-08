881dd85b5970d664f6270821a62e90a429c0394f
kortirso <kortirso@gmail.com>
2018-01-08 11:46:10 +0300

    add configuration

--- mix.exs
+++ mix.exs
@@ -1,28 +1,42 @@
 defmodule YandexTranslator.Mixfile do
   use Mix.Project
 
+  @description """
+    Translate strings using the Yandex Translator API. Requires that you have a Client ID. See README.md for information.
+  """
+
   def project do
     [
       app: :yandex_translator,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: @description,
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/kortirso/yandex_translator"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.0.0"},
+      {:poison, "~> 3.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Bogdanov Anton"],
+      licenses: ["MIT"],
+      links: %{"github" => "https://github.com/kortirso/yandex_translator"}
     ]
   end
 end
