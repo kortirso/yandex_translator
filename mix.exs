010394ae240cdc5d8e5b8632387752cd06a8aed9
kortirso <kortirso@gmail.com>
2019-01-30 14:51:03 +0300

    release 0.9.6

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule YandexTranslator.Mixfile do
   def project do
     [
       app: :yandex_translator,
-      version: "0.9.5",
+      version: "0.9.6",
       elixir: "~> 1.7",
       description: @description,
       build_embedded: Mix.env == :prod,
